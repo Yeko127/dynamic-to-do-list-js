5ce5ae3c68b8b638c2b6c3626dae8693f64fbdff
Carolyne Yeko <yekocarol2@gmail.com>
2025-11-30 13:26:13 +0300

    Added function for loading tasks from storage

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
-    function addTask () {
-        const taskText = taskInput.value.trim();
+    let tasks = [];
+    
+    function loadTasks() {
+        const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+        tasks = storedTasks;
 
-        if (taskText=== "") {
-            alert("Please enter a task.");
-            return;
-        }
+        storedTasks.forEach(taskText => createTaskElement(taskText, false));
+    }
 
+    function saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    function createTaskElement (taskText, save = true) {
         const li = document.createElement('li');
-        li.textContent = taskText;
+        li.textContent = taskText
 
         const removeBtn = document.createElement('button');
         removeBtn.textContent= "Remove"
@@ -20,11 +27,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
         removeBtn.onclick = function() {
             taskList.removeChild(li);
-        };
+
+            if (save) {
+                tasks = tasks.filter(task => task !== taskText);
+                saveTasks();
+            }
+    }; 
 
         li.appendChild(removeBtn);
         taskList.appendChild(li);
+        
+        if (save){
+            tasks.push(taskText);
+            saveTasks();
+        }
+     
+    }
 
+    function addTask() {
+        const taskText = taskInput.value.trim();
+        
+        if (taskText === "") {
+            alert("Please enter a task.")
+            return;
+        }
+
+        createTaskElement(taskText);
         taskInput.value = "";
     }
     
@@ -36,5 +64,5 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
     
-   
+   loadTasks();
 });
